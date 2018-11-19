2e6697fb53e302ec4ebeb3c38d4bf7e188b94b8c
HoIX <hoix103@gmail.com>
2018-11-19 22:01:10 +0800

    Override the Fragment’s onStop() method

--- app/src/main/java/com/example/android/miwok/NumbersFragment.java
+++ app/src/main/java/com/example/android/miwok/NumbersFragment.java
@@ -76,6 +76,14 @@ public class NumbersFragment extends Fragment {
         return textView;
     }
 
+    @Override
+    public void onStop() {
+        super.onStop();
+        // When the activity is stopped, release the media player resources because we won't
+        // be playing any more sounds.
+        releaseMediaPlayer();
+    }
+
     /**
      * Clean up the media player by releasing its resources.
      */
